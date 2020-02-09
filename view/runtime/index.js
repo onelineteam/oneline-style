75d279734a42b8a9f7aabd73d2b5e1609d3716b3
keep2zero <towaywu@gmail.com>
2020-02-09 21:49:45 +0800

    change: 优化运行代码

--- view/runtime/index.js
+++ view/runtime/index.js
@@ -3,6 +3,9 @@ const Watchpack = require('watchpack');
 const watch = new Watchpack({poll: true});
 const path = require('path');
 const fs = require('fs');
+const builder = require('../../bin/builder')
+
+
 const appFile = path.resolve(__dirname, '../../lib');
 const htmlFile = path.resolve(__dirname, '../pages');
 let socket = null;
@@ -11,6 +14,7 @@ let socket = null;
 watch.watch([], [appFile, htmlFile], Date.now());
 watch.on('change',(filePath) => {
   // console.log("文件改变", ":", filePath);
+  builder('pc', false);
   if(socket) {
     socket.send("true");
   }
@@ -26,8 +30,11 @@ const socketTemplate = `<script>
     }
     </script>
 `
+const cssTemplate = `
+  <link rel="stylesheet" href="/css/index.css" type="text/css">
+`
 
-const app = start(5000, {static: {prefix: "/view", path: path.resolve(__dirname, "../pages")}}, [], ()=>{
+const app = start(5000, {static: {prefix: "/css", path: path.resolve(__dirname, "../../dist")}}, [], ()=>{
   console.log("启动成功")
 })
 
@@ -36,9 +43,10 @@ app.get("/:page", (request, response)=>{
    const file = path.resolve(__dirname, "../pages/" + page);
    if(fs.existsSync(file)) {
     const fileData = fs.readFileSync(file);
-    console.log(response.type)
+     
     response.type("text/html")
     let content = fileData.toString();
+    content = content.replace("</head>", cssTemplate + "</head>");
     content = content.replace("</body>", socketTemplate + "</body>");
     response.send(content);
   } else {
